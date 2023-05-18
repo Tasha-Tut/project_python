ab425e187c46586cfed3473ea85774f960c63c0b
Natalia Cherevichnaya <95243594+Tasha-Tut@users.noreply.github.com>
2023-05-19 02:04:38 +0300

    Delete Python/Memory_Match/src directory

--- Python/Memory_Match/src/print_text.py
+++
@@ -1,6 +0,0 @@
-from global_varies import *
-
-def print_text(message, x, y, font_color = (0, 0, 0), font_type=font):
-    '''funtiont to print text'''
-    text = font_type.render(message, True, font_color)
-    screen.blit(text, (x, y))
