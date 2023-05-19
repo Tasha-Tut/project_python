fabf31a7adbb9377fe361ceaaaac36032fbf04c6
Tasha-Tut <na.km1973@gmail.com>
2023-05-19 12:39:21 +0300

    [memory match] initial commit

--- Python/Memory_Match/global_varies.py
+++ Python/Memory_Match/global_varies.py
@@ -1,24 +1,27 @@
 import pygame
 
+pygame.init()
+
 """materials"""
 WIDTH = 600
 HEIGHT = 600
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Matching Game")
-icon = pygame.image.load('images/icon.png').convert_alpha()
-pygame.display.set_icon(icon)
 font = pygame.font.Font('fonts/RobotoMono-Medium.ttf', 25)
 title_font = pygame.font.Font('fonts/RobotoMono-Medium.ttf', 40)
 bg_sound = pygame.mixer.Sound('music/lo-fi.mp3')
 
+
+timer = pygame.time.Clock()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
 """game variables and constance"""
 fps = 60
-timer = pygame.time.Clock()
 rows = 6
 cols = 8
 yellow = (245, 239, 56)
 correct = []
-new_board = True
+
+
 options_list = []
 spaces = []
 used = []
@@ -28,16 +31,10 @@ correct = [[0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0]]
-first_guess = False
-second_guess = False
-game_over = False
-first_guess_num = 0
-second_guess_num = 0
-score = 0
-best_score = 0
-matches = 0
+
+
 theme_now = 'black'
-running = True
+
 show_menu = True
 play_sound = True
 
@@ -55,3 +52,4 @@ blue = (6, 83, 184)
 black_theme = True
 black_back = (230, 231, 232)
 black = (0, 0, 0)
+
